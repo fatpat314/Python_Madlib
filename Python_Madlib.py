8e020a0a4979df91b7be32cd1c065a2096908933
Patrick <w.patrick.kelly@gmail.com>
2019-08-30 11:25:28 -0700

    Adding a list

--- Python_Madlib.py
+++ Python_Madlib.py
@@ -1,32 +1,36 @@
 
 
 
-compassDirection = input("Enter a Compass direction: ")
-City = input("Enter a city: ")
-place = input("Enter a place: ")
-verb = input("Enter a verb: ")
-verb2 = input("Enter a diffrent verb: ")
-place2 = input("Enter a diffrent place: ")
-activity = input("Enter your favorite activity: ")
-noun = input("Enter a noun: ")
-noun2 = input("Enter a diffrent noun: ")
-emotion = input("Enter an emotion: ")
-family = input("Enter a family member: ")
-family2 = input("Enter a diffrent family member: ")
-City2 = input("Enter a diffrent city: ")
+compassDirection = input("Enter a Compass direction: ") #0
+City = input("Enter a city: ") #1
+place = input("Enter a place: ") #2
+verb = input("Enter a verb: ") #3
+verb2 = input("Enter a diffrent verb: ") #4
+place2 = input("Enter a diffrent place: ") #5
+activity = input("Enter your favorite activity: ") #6
+noun = input("Enter a noun: ") #7
+noun2 = input("Enter a diffrent noun: ") #8
+emotion = input("Enter an emotion: ") #9
+family = input("Enter a family member: ") #10
+family2 = input("Enter a diffrent family member: ") #11
+City2 = input("Enter a diffrent city: ") #12
+
+Madlib_ls = [compassDirection, City, place, verb, verb2, place2, activity, noun, noun2, emotion, family, family2, City2]
 
 
 
 def function():
     print("====================================================")
-    print("In", compassDirection, City, "Born and raised")
-    print("on the", place,"was where I spent most of my days")
-    print("Chillin' out", verb+"in'", "relaxin' all cool")
-    print("and all",verb2,"some",activity,"outside of the",place2)
-    print("When a couple of", noun+"'s","who were up to no good")
-    print("Started making Trouble in my",place)
-    print("I got in one little",noun2,"and my mom got",emotion)
-    print("She said 'You're movin' with your", family, " and ", family2, " in ", City2+"'")
+    print("In", Madlib_ls[0], Madlib_ls[1], "Born and raised")
+    print("on the", Madlib_ls[2],"was where I spent most of my days")
+    print("Chillin' out", Madlib_ls[3]+"in'", "relaxin' all cool")
+    print("and all",Madlib_ls[4],"some",Madlib_ls[6],"outside of the",Madlib_ls[5])
+    print("When a couple of", Madlib_ls[7]+"'s","who were up to no good")
+    print("Started making Trouble in my neighborhood")
+    print("I got in one little",Madlib_ls[8],"and my mom got",Madlib_ls[9])
+    print("She said 'You're movin' with your", Madlib_ls[10], " and ", Madlib_ls[11], " in ", Madlib_ls[12]+"'")
     print("====================================================")
 
 function()
+
+# Add a list
