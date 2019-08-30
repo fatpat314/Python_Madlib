956982791b09c0a6ea400987320b4ba964f78d67
Patrick <w.patrick.kelly@gmail.com>
2019-08-30 10:59:24 -0700

    Add a function

--- Python_Madlib.py
+++ Python_Madlib.py
@@ -16,13 +16,17 @@ family2 = input("Enter a diffrent family member: ")
 City2 = input("Enter a diffrent city: ")
 
 
-print("====================================================")
-print("In", compassDirection, City, "Born and raised")
-print("on the", place,"was where I spent most of my days")
-print("Chillin' out", verb+"in'", "relaxin' all cool")
-print("and all",verb2,"some",activity,"outside of the",place2)
-print("When a couple of", noun+"'s","who were up to no good")
-print("Started making Trouble in my",place)
-print("I got in one little",noun2,"and my mom got",emotion)
-print("She said 'You're movin' with your", family, " and ", family2, " in ", City2+"'")
-print("====================================================")
+
+def function():
+    print("====================================================")
+    print("In", compassDirection, City, "Born and raised")
+    print("on the", place,"was where I spent most of my days")
+    print("Chillin' out", verb+"in'", "relaxin' all cool")
+    print("and all",verb2,"some",activity,"outside of the",place2)
+    print("When a couple of", noun+"'s","who were up to no good")
+    print("Started making Trouble in my",place)
+    print("I got in one little",noun2,"and my mom got",emotion)
+    print("She said 'You're movin' with your", family, " and ", family2, " in ", City2+"'")
+    print("====================================================")
+
+function()
