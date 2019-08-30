6f37bc59547a0f1efb58f2dd7d98126242e4bee9
fatpat314 <w.patrick.kelly@gmail.com>
2019-08-29 20:51:39 -0700

    Add files via upload

---
+++ Python_Madlib.py
@@ -0,0 +1,27 @@
+
+
+
+compassDirection = input("Enter a Compass direction: ")
+City = input("Enter a city: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+verb2 = input("Enter a diffrent verb: ")
+place2 = input("Enter a diffrent place: ")
+activity = input("Enter your favorite activity: ")
+noun = input("Enter a noun: ")
+noun2 = input("Enter a diffrent noun: ")
+emotion = input("Enter an emotion: ")
+family = input("Enter a family member: ")
+family2 = input("Enter a diffrent family member: ")
+City2 = input("Enter a diffrent city: ")
+
+
+print("====================================================")
+print("In", compassDirection, City, "Born and raised")
+print("on the", place,"was where I spent most of my days")
+print("Chillin' out", verb+"in'", "relaxin' all cool")
+print("and all",verb2,"some",activity,"outside of the",place2)
+print("When a couple of", noun+"'s","who were up to no good")
+print("Started making Trouble in my",place)
+print("I got in one little",noun2,"and my mom got",emotion)
+print("She said 'You're movin' with your", family, " and ", family2, " in ", City2)
